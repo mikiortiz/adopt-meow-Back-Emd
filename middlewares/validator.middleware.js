55265a3c194363efe9f2c3187b6b538552a120e2
Miguel Ortiz <ortizmichel390@gmail.com>
2024-05-26 14:15:39 -0300

    User Authentication and Validation Update

---
+++ middlewares/validator.middleware.js
@@ -0,0 +1,18 @@
+const validatorSchema = (schema) => (req, res, next) => {
+  try {
+    schema.validateSync(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errors = error.inner.map((err) => ({
+        field: err.path,
+        message: err.message,
+      }));
+      return res.status(400).json({ errors });
+    } else {
+      return res.status(500).json({ message: "Error interno del servidor" });
+    }
+  }
+};
+
+module.exports = validatorSchema;
